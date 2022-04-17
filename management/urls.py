164dfc9bbb06f20fd12db964ec867adc4101032b
MK3247 <wafulacharles47@gmail.com>
2022-04-17 13:24:51 +0300

    git hub login

--- management/urls.py
+++ management/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('oauth/', include('social_django.urls')),
     path('account/', include('users.urls')),
 ]
